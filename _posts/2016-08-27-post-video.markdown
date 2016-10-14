3bf9e517e96b864ccef3c2f731da427e47db6e36
Chris Muccioli <chris@kickstarter.com>
2016-10-14 16:24:50 -0400

    Fit Vid + Post Testing

--- _posts/2016-08-27-post-video.markdown
+++ _posts/2016-08-27-post-video.markdown
@@ -3,7 +3,8 @@ layout: post
 title:  "Video Test"
 date:   2016-08-26 15:54:39 -0400
 categories: video
+roles: Director / DP / Editor
+credits: Escobedo Soliz – MoMA
 ---
-<a href="http://www.youtube.com/watch?feature=player_embedded&v=YOUTUBE_VIDEO_ID_HERE
-" target="_blank"><img src="http://img.youtube.com/vi/YOUTUBE_VIDEO_ID_HERE/0.jpg"
-alt="IMAGE ALT TEXT HERE" width="240" height="180" border="10" /></a>
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/FU3UYJD69Pk" frameborder="0" allowfullscreen></iframe>
--- js/js.js
+++ js/js.js
@@ -1,11 +1,30 @@
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1000) {
-        $(".bottom-nav").stop().animate({
-            opacity: 1
-        }, 500);
-    } else {
-        $(".bottom-nav").stop().animate({
-            opacity: 0
-        }, 500);
-    }
-});
+// $(function() {
+
+//     var $allVideos = $("iframe[src^='//player.vimeo.com'], iframe[src^='//www.youtube.com'], object, embed"),
+//     $fluidEl = $("figure");
+
+//   $allVideos.each(function() {
+
+//     $(this)
+//       // jQuery .data does not work on object/embed elements
+//       .attr('data-aspectRatio', this.height / this.width)
+//       .removeAttr('height')
+//       .removeAttr('width');
+
+//   });
+
+//   $(window).resize(function() {
+
+//     var newWidth = $fluidEl.width();
+//     $allVideos.each(function() {
+
+//       var $el = $(this);
+//       $el
+//           .width(newWidth)
+//           .height(newWidth * $el.attr('data-aspectRatio'));
+
+//     });
+
+//   }).resize();
+
+// });
